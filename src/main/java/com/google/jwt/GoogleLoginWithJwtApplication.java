085c392c9156b017e9a6ab2224cb38d0e142a4b3
mangojuicedeli <mgkang0317@gmail.com>
2020-08-19 14:38:54 +0900

    initial commit

---
+++ src/main/java/com/google/jwt/GoogleLoginWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.google.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleLoginWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/jwt/GoogleLoginWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
